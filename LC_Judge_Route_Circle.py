a78ae48beb4ee2cc709396fa089d07ef33e794e8
Venkata Krishnan Anantha Raman <va34@buffalo.edu>
2017-12-20 17:32:27 -0500

    Judge Route Circle Problem

---
+++ LC_Judge_Route_Circle.py
@@ -0,0 +1,32 @@
+# Judge Route Circle
+# https://leetcode.com/problems/number-of-islands/description/
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        n = 0
+        m = len(grid)
+        if (m >0):
+            n = len(grid[0])
+        
+        if (m == n):
+            if (n == 0):
+                return 0
+        
+        def modified_dfs (i,j):
+            if ((0<=i<m) and (0<=j<n) and (grid[i][j]== "1")):
+                grid[i][j] = 0
+                modified_dfs (i-1 , j)
+                modified_dfs (i+1 , j)
+                modified_dfs (i, j+1)
+                modified_dfs (i, j-1)
+                return 1
+            else:
+                return 0
+        
+       
+    
+        num_of_islands = [modified_dfs(i,j) for i in range(m) for j in range(n) if grid[i][j]]
+        return sum(num_of_islands)
