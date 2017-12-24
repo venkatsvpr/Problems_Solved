d1a1d81390c0764f24cb9fff06ff4efe909ecd4b
Venkata Krishnan Anantha Raman <va34@buffalo.edu>
2017-12-23 21:41:57 -0500

    Problems solved

---
+++ LC_Merge_Intervals2.py
@@ -0,0 +1,17 @@
+# Merge Intervals
+# https://leetcode.com/problems/merge-intervals/description/
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    def merge(self, intervals):
+        out = []
+        for i in sorted(intervals, key=lambda x: x.start):
+            if out and i.start <= out[-1].end:
+                out[-1].end = max(out[-1].end, i.end)
+            else:
+                out.append(i)
+        return out
