2acbe51645424a3bfb913fb024a463bdc3b9f315
Venkata Krishnan Anantha Raman <va34@buffalo.edu>
2018-01-20 20:42:42 -0500

    Move Zeros

---
+++ LC_Move_Zeros.py
@@ -0,0 +1,31 @@
+"""
+Linearly search the nums and store the position of the zero
+Swap the position when we find a non-zero number.
+"""
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if not nums:
+            return 
+        zero_pos  = -1
+        index = 0
+        while (index <len(nums)): 
+            if (nums[index] != 0):
+                if (zero_pos != -1):
+                    nums[index],nums[zero_pos] = nums[zero_pos],nums[index]
+                    index = zero_pos + 1
+                    zero_pos = -1
+                    continue;                    
+                index = index +1
+                continue;
+            else:
+                if (zero_pos != -1):
+                    index = index+1
+                    continue;
+                zero_pos = index
+                index = index+1
+                continue;
+        return
