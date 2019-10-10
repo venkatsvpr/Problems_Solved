1f146afba7e60c853c4f9a847efacef4eaa202a6
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2019-10-09 23:38:13 -0700

    Problems for the last and current week

---
+++ LC_Unique_Number_Of_Occurances.go
@@ -0,0 +1,44 @@
+```
+1207. Unique Number of Occurrences
+
+Given an array of integers arr, write a function that returns true if and only if the number of occurrences of each value in the array is unique.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,1,1,3]
+Output: true
+Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+Example 2:
+
+Input: arr = [1,2]
+Output: false
+Example 3:
+
+Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+Output: true
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+-1000 <= arr[i] <= 1000
+```
+
+
+func uniqueOccurrences(arr []int) bool {
+    freq := make(map[int] int)
+    for _,item := range(arr) {
+        freq[item] += 1
+    }
+    freqSet := make(map[int] bool)
+    for key := range(freq) {
+        if (freqSet[freq[key]]) {
+            return false
+        } else {
+            freqSet[freq[key]] = true
+        }
+    }
+    return true
+}
