840e78b6c691ddfe050654d2cfa2fbbe441a2496
Venkata Krishnan Anantha Raman <venkatakrishnansvpr@gmail.com>
2020-08-02 12:20:49 -0700

    Problems for the day

---
+++ LC_Count_Good_Triplets.py
@@ -0,0 +1,63 @@
+"""
+1528. Shuffle String
+
+Given a string s and an integer array indices of the same length.
+
+The string s will be shuffled such that the character at the ith position moves to indices[i] in the shuffled string.
+
+Return the shuffled string.
+
+ 
+
+Example 1:
+
+
+Input: s = "codeleet", indices = [4,5,6,7,0,2,1,3]
+Output: "leetcode"
+Explanation: As shown, "codeleet" becomes "leetcode" after shuffling.
+Example 2:
+
+Input: s = "abc", indices = [0,1,2]
+Output: "abc"
+Explanation: After shuffling, each character remains in its position.
+Example 3:
+
+Input: s = "aiohn", indices = [3,1,4,2,0]
+Output: "nihao"
+Example 4:
+
+Input: s = "aaiougrt", indices = [4,0,2,6,7,3,1,5]
+Output: "arigatou"
+Example 5:
+
+Input: s = "art", indices = [1,0,2]
+Output: "rat"
+ 
+
+Constraints:
+
+s.length == indices.length == n
+1 <= n <= 100
+s contains only lower-case English letters.
+0 <= indices[i] < n
+All values of indices are unique (i.e. indices is a permutation of the integers from 0 to n - 1).
+
+"""
+
+class Solution(object):
+    def countGoodTriplets(self, arr, a, b, c):
+        """
+        :type arr: List[int]
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(arr)-2):
+            for j in range(i+1, len(arr)-1):
+                if (abs(arr[i] - arr[j]) <= a):
+                    for k in range(j+1, len(arr)):
+                        if (abs(arr[j] - arr[k]) <= b) and (abs(arr[i] - arr[k]) <= c):
+                            count += 1
+        return count
