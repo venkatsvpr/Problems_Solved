0143bbdd81a8d2138806ab866527b7bd53db800c
Venkata Krishnan <venkatakrishnansvpr@gmail.com>
2022-02-13 11:19:10 -0800

    Create LC_Maximize_the_confusion_of_an_exam.py

---
+++ LC_Maximize_the_confusion_of_an_exam.py
@@ -0,0 +1,64 @@
+"""
+2024. Maximize the Confusion of an Exam
+
+A teacher is writing a test with n true/false questions, with 'T' denoting true and 'F' denoting false. He wants to confuse the students by maximizing the number of consecutive questions with the same answer (multiple trues or multiple falses in a row).
+
+You are given a string answerKey, where answerKey[i] is the original answer to the ith question. In addition, you are given an integer k, the maximum number of times you may perform the following operation:
+
+Change the answer key for any question to 'T' or 'F' (i.e., set answerKey[i] to 'T' or 'F').
+Return the maximum number of consecutive 'T's or 'F's in the answer key after performing the operation at most k times.
+
+ 
+
+Example 1:
+
+Input: answerKey = "TTFF", k = 2
+Output: 4
+Explanation: We can replace both the 'F's with 'T's to make answerKey = "TTTT".
+There are four consecutive 'T's.
+Example 2:
+
+Input: answerKey = "TFFT", k = 1
+Output: 3
+Explanation: We can replace the first 'T' with an 'F' to make answerKey = "FFFT".
+Alternatively, we can replace the second 'T' with an 'F' to make answerKey = "TFFF".
+In both cases, there are three consecutive 'F's.
+Example 3:
+
+Input: answerKey = "TTFTTFTT", k = 1
+Output: 5
+Explanation: We can replace the first 'F' to make answerKey = "TTTTTFTT"
+Alternatively, we can replace the second 'F' to make answerKey = "TTFTTTTT". 
+In both cases, there are five consecutive 'T's.
+ 
+
+Constraints:
+
+n == answerKey.length
+1 <= n <= 5 * 104
+answerKey[i] is either 'T' or 'F'
+1 <= k <= n
+
+"""
+class Solution(object):
+    def maxConsecutiveAnswers(self, answerKey, k):
+        """
+        :type answerKey: str
+        :type k: int
+        :rtype: int
+        """
+        def findMax(ans):
+            cur = 0
+            res = 0
+            d = dict()
+            ## Store the idx when zero of T/F was found
+            d[0] = -1
+            for idx,ch in enumerate(answerKey):
+                if ch != ans:
+                    cur += 1
+                    d[cur] = idx
+                # The reason for doing cur - k is to find length of curr and zero occurance 
+                res = max(res, idx - d[max(0, cur-k)])
+            return res
+        return max(findMax("T"), findMax("F"))
+                
