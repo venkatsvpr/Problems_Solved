b6b9e55286be87a2cd7beb4ed4650a41a738c7f0
Venkata Krishnan <venkatakrishnansvpr@gmail.com>
2022-03-09 22:48:04 -0800

    Create LC_Partition_Equal_Subset_Sum.py

---
+++ LC_Partition_Equal_Subset_Sum.py
@@ -0,0 +1,53 @@
+"""
+416. Partition Equal Subset Sum
+Given a non-empty array nums containing only positive integers, find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal.
+
+ 
+
+Example 1:
+
+Input: nums = [1,5,11,5]
+Output: true
+Explanation: The array can be partitioned as [1, 5, 5] and [11].
+Example 2:
+
+Input: nums = [1,2,3,5]
+Output: false
+Explanation: The array cannot be partitioned into equal sum subsets.
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
+"""
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        def hasSubset (s):
+            n = len(nums)
+            dp = [[False for j in range(s+1)] for i in range(n+1)]
+            for i in range(n+1):
+                for j in range(s+1):
+                    if j == 0:
+                        dp[i][j] = True
+            
+            for i in range (1,n+1):
+                for j in range(1,s+1):
+                    # If the number is big we cannot do anything, so check the same sum without the number
+                    if nums[i-1] > j:
+                        dp[i][j] = dp[i-1][j]
+                    else:
+                        # we have two decisions, pick it or not pick it
+                        # if we pick, the check the best value for newSum = j - nums[i] in the nums without the current number
+                        dp[i][j] = dp[i-1][j-nums[i-1]] or dp[i-1][j]
+            return dp[len(nums)][s]
+        
+        # To do this the sum should be even
+        if sum(nums) % 2 != 0 :
+            return False
+        
+        return hasSubset(sum(nums)/2)
